92a13b84cfd4be96aa6d0a8f763617db442d5470
Mohammad Kaif <mkaif1617@gmail.com>
2025-07-29 15:17:44 +0530

    feat: configure initial TypeScript server with MongoDB connection

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+// src/index.ts
+import dotenv from 'dotenv';
+dotenv.config(); // This must be the first line to load .env variables
+
+import express, {Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// --- 1. Middleware ---
+app.use(express.json());
+
+
+// --- 2. Database Connection ---
+const mongoUri = process.env.MONGO_URI as string; 
+
+if (!mongoUri) {
+  console.error('FATAL ERROR: MONGO_URI is not defined.');
+  process.exit(1);
+}
+
+mongoose.connect(mongoUri)
+  .then(() => console.log('Successfully connected to MongoDB Atlas!'))
+  .catch(err => console.error('Connection error', err));
+
+
+// --- 3. Basic Routes ---
+app.get('/', (req: Request, res: Response) => {
+  res.send('LMS Backend is up and running!');
+});
+
+
+// --- 4. Start the Server ---
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
